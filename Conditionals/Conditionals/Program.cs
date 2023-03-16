e7a6ab3732f28041f0992aff2be22250a93fd8de
VAN1LLA <tlink1776@gmail.com>
2023-03-16 12:43:26 -0400

    Continued development

---
+++ Conditionals/Conditionals/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Conditionals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = 10;
+
+            if(hour > 0 && hour < 12)
+            {
+                Console.WriteLine("It's morning!");
+            }else if (hour >= 12 && hour < 18)
+            {
+                Console.WriteLine("It's evening!");
+            }else
+            {
+                Console.WriteLine("It's night!");
+            }
+
+            section2();
+            section3();
+            SeasonController.section4();
+        }
+
+        public static void section2()
+        {
+            bool isGoldCustomer = true;
+            float price;
+
+            if(isGoldCustomer)
+            {
+                price= 19.95f;
+            }else
+            {
+                price = 29.95f;
+            }
+
+            Console.WriteLine(price);
+        }
+
+        public static void section3()
+        {
+            bool isGoldCustomer = false;
+            
+            /*
+             * float price = declare a variable
+             * (isGoldCustomer) = conditional statement
+             * ? = stating that this is a conditional statement
+             * 19.95f = if conditional succeeds
+             * 29.95f = if conditional does not succeed 
+             */
+            float price = (isGoldCustomer) ? 19.95f : 29.95f;
+            Console.WriteLine(price);
+        }
+    }
+}
---
+++ Conditionals/Conditionals/SeasonController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditionals
+{
+    internal class SeasonController
+    {
+        public static void section4()
+        {
+            var season = Season.Fall;
+
+            switch (season)
+            {
+                case Season.Fall:
+                    Console.WriteLine("It's fall. Time for football.");
+                    break;
+                case Season.Summer:
+                    Console.WriteLine("It's summer. Time for baseball.");
+                    break;
+                default: 
+                    Console.WriteLine("I don't know that season.");
+                    break;
+            }
+
+            // The following is read as: If case is Fall or Summer. 
+            switch (season)
+            {
+                case Season.Fall:
+                case Season.Summer:
+                    Console.WriteLine("This is a block for Fall or Summer.");
+                    break;
+                default:
+                    Console.WriteLine("I don't know that season.");
+                    break;
+
+            }
+        }
+    }
+}
